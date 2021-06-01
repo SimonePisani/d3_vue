37f21ad76ac2cb3795fcc169989d4aaa3d971475
SimonePisani <simonepisani11@gmail.com>
2021-06-01 18:03:17 +0200

    Introducing 3d.js library and first visualization

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>D3 visualization within Vue</h1>
+    <svg id="viz" height="600" width="800"></svg>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const d3 = require('d3');
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  mounted() {
+    let numbers=[150,250,160,80,200];
+
+    //  select the visual environment
+    const svg = d3.select('#viz');
+    //  join my data
+    const rects = svg.selectAll('rect')
+        .data(numbers)
+        .join('rect');
+    //  enter + exit + update: join()
+    const scaleLenght = d3.scaleLinear()
+        // setting a min and a max value
+        .domain([0, d3.max(numbers)])
+        // setting a min and a max value for our rappresentation
+        .range([0, 400]);
+
+    const scalePosition = function (d, i){
+      return 20 * i + 20;
+    }
+
+    rects
+        .attr('x', 20)
+        .attr('height', 19)
+        .attr('y', scalePosition)
+        .attr('width', scaleLenght)
   }
 }
 </script>
@@ -21,7 +47,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
