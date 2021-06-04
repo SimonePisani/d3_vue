ad4d2c08b3fdb174205901e263523490e78f665a
SimonePisani <simonepisani11@gmail.com>
2021-06-04 17:33:20 +0200

    Optimization of the code & cascading selection

--- src/App.vue
+++ src/App.vue
@@ -13,55 +13,78 @@ export default {
   components: {
 
   },
-  mounted: function () {
-    let numbers = [150, 250, 160, 80, 200, 300, 120, 240, 1000, 13];
+  // store is an element that generate a data section that stores all data of my component. Is a function that
+  data(){
+    return {
+      numbers: [100, 250, 160, 80, 200]
+    }
+  },
 
-    //  select the visual environment
-    const svg = d3.select('#viz');
+  mounted() {
+    this.refreshChart(this.numbers)
+  },
+  // i can put here functions that are named with one of the properties of our data, in this case for example numbers, and this function are passed two values, a new val and an old val. Everytime we change one of the element in our vue, vue will notify the function and change the values
+  watch: {
+    numbers(newVal){
+      this.refreshChart(newVal);
+    }
+  },
+  methods: {
+    refreshChart(listOfNumbers){
+      //  select the visual environment
+      const svg = d3.select('#viz');
 
-    const scaleLenght = d3.scaleLinear()
-        // setting a min and a max value
-        .domain([0, d3.max(numbers)])
-        // setting a min and a max value for our representation
-        .range([0, 600]);
+      const scaleLenght = d3.scaleLinear()
+          // setting a min and a max value
+          .domain([0, d3.max(listOfNumbers)])
+          // setting a min and a max value for our representation
+          .range([0, 600]);
 
-    const lAxis = d3.axisTop(scaleLenght);
+      const lAxis = d3.axisTop(scaleLenght);
 
-    const scalePos = d3.scaleBand()
-        //setting our ordinal values
-        .domain(d3.range(numbers.length))
-        //setting a min and a max values for our visual representations
-        .range([0, 300])
-        //delete any decimal values
-        .round(true)
-        //apply a padding to our visual representations
-        .paddingInner(0.05)
-        .paddingOuter(0.05);
-
-
-    // ******* Create g groups ********
-    svg.append('g')
-        .attr('class', 'lAxis')
-        .attr('transform', 'translate(20, 20)')
-        .call(lAxis);
+      const scalePos = d3.scaleBand()
+          //setting our ordinal values
+          .domain(d3.range(listOfNumbers.length))
+          //setting a min and a max values for our visual representations
+          .range([0, 300])
+          //delete any decimal values
+          .round(true)
+          //apply a padding to our visual representations
+          .paddingInner(0.05)
+          .paddingOuter(0.05);
 
 
-    // G is an element widely used in visual representation, each g contains one of our bars, we arent getting all the possible gs within our visualization but only those that have a specific class called "bars".
-    const gs = svg.selectAll('g.bars')
-        .data(numbers)
-        .join('g').attr('class', 'bars');
+      // ******* Create g groups ********
+      svg.selectAll('g.lAxis')
+          .data([0])
+          .join('g')
+          .attr('class', 'lAxis')
+          .attr('transform', 'translate(20, 20)')
+          .call(lAxis);
 
-    gs.attr('transform', (d,i) => `translate(20, ${30 + scalePos(i)})`);
 
-    gs.append('rect')
-        .attr('height', scalePos.bandwidth())
-        .attr('width', scaleLenght)
-        .attr('fill', '#0c8e8e');
+      // G is an element widely used in visual representation, each g contains one of our bars, we arent getting all the possible gs within our visualization but only those that have a specific class called "bars".
+      const gs = svg.selectAll('g.bars')
+          .data(listOfNumbers)
+          .join('g').attr('class', 'bars');
 
-    gs.append('text')
-        .text((d) => d)
-        .attr('y', scalePos.bandwidth()/2)
-        .attr('x', scaleLenght);
+      gs.attr('transform', (d,i) => `translate(20, ${30 + scalePos(i)})`);
+
+      gs.selectAll('rect')
+          .data((d) => [d])
+          .join('rect')
+          .attr('height', scalePos.bandwidth())
+          .attr('width', scaleLenght)
+          .attr('fill', '#0c8e8e');
+
+      gs.selectAll('text')
+          .data(d => [d])
+          .join('text')
+          .text((d) => d)
+          .attr('y', scalePos.bandwidth()/2)
+          .attr('x', scaleLenght);
+    }
+
   }
 }
 </script>
