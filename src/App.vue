aa3e525076ca5c8553f75b182418229e73ac3658
SimonePisani <simonepisani11@gmail.com>
2021-06-11 17:13:09 +0200

    adding width and height properties to barchart component

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       </b-row>
       <b-row class="mt-3">
         <b-col>
-          <p>Click to shuffle your numbers</p>
+          <p><u>Click to shuffle your numbers</u></p>
         </b-col>
         <b-col>
           <b-button @click="shuffleNumbers()">Shuffle</b-button>
@@ -16,7 +16,7 @@
       </b-row>
       <b-row>
         <b-col class="mt-5">
-          <svg id="viz" height="600" width="800"></svg>
+          <svg id="viz" height="600" width="900"></svg>
         </b-col>
       </b-row>
     </b-container>
@@ -25,6 +25,9 @@
 
 <script>
 const d3 = require('d3');
+
+import chart from "@/assets/js/barchartvisualization";
+
 
 export default {
   name: 'App',
@@ -50,61 +53,12 @@ export default {
   methods: {
     refreshChart(listOfNumbers){
       //  select the visual environment
-      const svg = d3.select('#viz');
-
-      const scaleLenght = d3.scaleLinear()
-          // setting a min and a max value
-          .domain([0, d3.max(listOfNumbers)])
-          // setting a min and a max value for our representation
-          .range([0, 600]);
-
-      const lAxis = d3.axisTop(scaleLenght);
-
-      const scalePos = d3.scaleBand()
-          //setting our ordinal values
-          .domain(d3.range(listOfNumbers.length))
-          //setting a min and a max values for our visual representations
-          .range([0, 300])
-          //delete any decimal values
-          .round(true)
-          //apply a padding to our visual representations
-          .paddingInner(0.05)
-          .paddingOuter(0.05);
-
-
-      // ******* Create g groups ********
-      svg.selectAll('g.lAxis')
-          .data([0])
-          .join('g')
-          .attr('class', 'lAxis')
-          .attr('transform', 'translate(20, 20)')
-          .call(lAxis);
-
-
-      // G is an element widely used in visual representation, each g contains one of our bars, we arent getting all the possible gs within our visualization but only those that have a specific class called "bars".
-      const gs = svg.selectAll('g.bars')
-          .data(listOfNumbers)
-          .join('g').attr('class', 'bars');
-
-      gs.attr('transform', (d,i) => `translate(20, ${30 + scalePos(i)})`);
-
-      gs.selectAll('rect')
-          .data((d) => [d])
-          .join('rect')
-          .attr('height', scalePos.bandwidth())
-          .attr('width', scaleLenght)
-          .attr('fill', '#0c8e8e');
-
-      gs.selectAll('text')
-          .data(d => [d])
-          .join('text')
-          .text((d) => d)
-          .attr('y', scalePos.bandwidth()/2)
-          .attr('x', scaleLenght);
+      const barChart = chart().width(800).height(600);
+      d3.select('#viz').datum(listOfNumbers).call(barChart);
     },
-    shuffleNumbers(){
-      const N = Math.round(Math.random()*10);
-      this.numbers = d3.range(N).map(d => Math.round(d+Math.random()*400));
+    shuffleNumbers() {
+      const N = Math.round(Math.random() * 10);
+      this.numbers = d3.range(N).map(d => Math.round(d + Math.random() * 400));
     }
   }
 }
---
+++ src/assets/js/barchartvisualization.js
@@ -0,0 +1,80 @@
+const d3 = require('d3');
+
+function chart(){
+    let width = 600;
+    let height = 300;
+
+    const scaleLenght = d3.scaleLinear()
+        // setting a min and a max value
+        // .domain([0, d3.max(numbers)])
+        // setting a min and a max value for our representation
+        .range([0, width]);
+
+    const lAxis = d3.axisTop(scaleLenght);
+
+    const scalePos = d3.scaleBand()
+        //setting our ordinal values
+        // .domain(d3.range(numbers.length))
+        //setting a min and a max values for our visual representations
+        .range([0, height])
+        //delete any decimal values
+        .round(true)
+        //apply a padding to our visual representations
+        .paddingInner(0.05)
+        .paddingOuter(0.05);
+
+    function my(selection){
+        let numbers = selection.datum();
+
+        scaleLenght.domain([0, d3.max(numbers)]);
+        scalePos.domain(d3.range(numbers.length));
+
+        // ******* Create g groups ********
+        selection.selectAll('g.lAxis')
+            .data([0])
+            .join('g')
+            .attr('class', 'lAxis')
+            .attr('transform', 'translate(20, 20)')
+            .call(lAxis);
+
+
+        // G is an element widely used in visual representation, each g contains one of our bars, we arent getting all the possible gs within our visualization but only those that have a specific class called "bars".
+        const gs = selection .selectAll('g.bars')
+            .data(numbers)
+            .join('g').attr('class', 'bars');
+
+        gs.attr('transform', (d,i) => `translate(20, ${30 + scalePos(i)})`);
+
+        gs.selectAll('rect')
+            .data((d) => [d])
+            .join('rect')
+            .attr('height', scalePos.bandwidth())
+            .attr('width', scaleLenght)
+            .attr('fill', '#0c8e8e');
+
+        gs.selectAll('text')
+            .data(d => [d])
+            .join('text')
+            .text((d) => d)
+            .attr('y', scalePos.bandwidth()/2)
+            .attr('x', scaleLenght);
+    }
+
+    my.width = function(value){
+        if(!arguments.length) return width;
+        width = value;
+        scaleLenght.range([0, width]);
+        return my;
+    }
+
+    my.height = function (value){
+        if(!arguments.length) return height;
+        height = value;
+        scalePos.range([0, height]);
+        return my;
+    }
+
+    return my;
+}
+
+ export default chart;
